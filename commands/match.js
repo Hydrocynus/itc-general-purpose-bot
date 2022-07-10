64b0633beccf5c104d1c2306e6a8a67e210bdcfa
Hydrocynus <tobias.ulmer@itc-studenten.de>
2022-07-10 02:10:14 +0200

    restructured config.json + way too many bufixes

--- commands/match.js
+++ commands/match.js
@@ -1,28 +1,38 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { request } = require('undici');
-const { modApi } = require('../config.json');
+const { minecraft: { modApi } } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('match')
 		.setDescription('Matches a Minecraft username to a Discord user ID')
-    .addStringOption(option => option.setName('minecraftUserName').setRequired(true).setDescription('The Minecraft username to match'))
-    .addUserOption(option => option.setName('discordUser').setRequired(true).setDescription('The Discord user to match')),
+    .addStringOption(option => option.setName('mcusername').setRequired(true).setDescription('The Minecraft username to match'))
+    .addUserOption(option => option.setName('discorduser').setRequired(true).setDescription('The Discord user to match')),
 	async execute(interaction) {
-		await interaction.deferredReply('Matching Minecraft username to Discord user ID...');
-		const uri = modApi + "/playerlist/match";
-		const { statusCode, headers, body} = await request(uri, {
-			method: "POST",
-			body: JSON.stringify({
-				minecraftUserName: interaction.getStringOption('minecraftUserName'),
-				discordUserID: interaction.getUserOption('discordUser').id
-			})
+		await interaction.deferReply('Matching Minecraft username to Discord user ID...');
+		console.log("match:", {
+			minecraftUserName: interaction.options.getString('mcusername'),
+			discordUserID: interaction.options.getUser('discorduser')?.id
 		});
-		if (`${statusCode}`.match(/^2\d\d$/)) {
-			await interaction.editReply(`${interaction.getStringOption('minecraftUserName')} is linked to ${interaction.getUserOption('discordUser').tag}`);
-		} else {
+		const uri = modApi + "/playerlist/match";
+		try {
+			const { statusCode, headers, body} = await request(uri, {
+				method: "POST",
+				body: JSON.stringify({
+					minecraftUserName: interaction.options.getString('mcusername'),
+					discordUserID: interaction.options.getUser('discorduser')?.id
+				})
+			})
+	
+			if (`${statusCode}`.match(/^2\d\d$/)) {
+				await interaction.editReply(`${interaction.getStringOption('minecraftUserName')} is linked to ${interaction.getUserOption('discordUser').tag}`);
+			} else {
+				await interaction.editReply("An error occurred");
+				console.error("Error while matching Minecraft username to Discord user ID:", body);
+			}
+		} catch (e) {
 			await interaction.editReply("An error occurred");
-			console.error(`Error while matching Minecraft username to Discord user ID: ${body}`);
+			console.error("Error while matching Minecraft username to Discord user ID:", e);
 		}
 	},
 };
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,21 +1,24 @@
-const { channelId, modApi } = require('../config.json');
+const { minecraft: { channelId, modApi } } = require('../config.json');
 const { request } = require('undici');
 
 module.exports = {
 	name: 'messageCreate',
 	execute: async message => {
-		if (message.channelId === channelId) return;
+		if (message.channelId !== channelId) return;
 
 		console.log(`${message.author.tag} said: ${message.content}`);
 
-		const { statusCode, headers, body} = await request(modApi + "/message", {
-			method: "POST",
-			body: JSON.stringify({
-				user: message.author.id,
-				text: message.content
-			})
-		});
-
-		console.log(`Mod answered with status code ${statusCode}:`, body);
+		try {
+			const { statusCode, headers, body} = await request(modApi + "/message", {
+				method: "POST",
+				body: JSON.stringify({
+					user: message.author.id,
+					text: message.content
+				})
+			});
+			console.log(`Mod answered with status code ${statusCode}:`, body);
+		} catch (e) {
+			console.error("Error while sending message to mod:", e);
+		}
 	}
 }
